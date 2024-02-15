aa178f81017873b4b4b062e401fce8bcd99f573d
JohnSaita <89454029+JohnSaita@users.noreply.github.com>
2024-02-15 13:41:46 +0300

    Add files via upload

---
+++ styles/schools_in_majimoto_1_style.js
@@ -0,0 +1,49 @@
+var size = 0;
+var placement = 'point';
+
+var style_schools_in_majimoto_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "2.6px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if (feature.get("Schools") !== null) {
+            labelText = String(feature.get("Schools"));
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [512, 512],
+                  scale: 0.1484375,
+                  anchor: [38, 38],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/school-svgrepo-com.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
